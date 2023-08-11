5d4919c967249604fb3c926fea77b3251dc1e5ab
Brijeshp18 <brijeshbri18@gmail.com>
2023-08-11 12:50:35 +0530

    initial commit

--- src/LayooutRoutes/Route.js
+++ src/LayooutRoutes/Route.js
@@ -13,7 +13,7 @@ function RouteLayout() {
        <Header/>
        <Routes >
        <Route path="/" element={<Logup/>} />
-       <Route path="/ecommerce" element={<Logup/>} />
+       <Route path="/techflix-store" element={<Logup/>} />
   
        <Route path="/product/:id" element={<ProductDetails/>} />
         
